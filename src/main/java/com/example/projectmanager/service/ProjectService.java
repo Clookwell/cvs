953bd9caf1eee6c3bc5cafb6aeb20207bb405a04
RomanPav <65815802+Clookwell@users.noreply.github.com>
2025-06-22 22:06:25 +0300

    Create ProjectService.java

---
+++ src/main/java/com/example/projectmanager/service/ProjectService.java
@@ -0,0 +1,24 @@
+package com.example.projectmanager.service;
+
+import com.example.projectmanager.model.Project;
+import com.example.projectmanager.repository.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public Project createProject(Project project) {
+        return projectRepository.save(project);
+    }
+}
